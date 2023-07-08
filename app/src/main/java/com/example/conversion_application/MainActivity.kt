1f7ffd559c2e9f0dcf3937d5c31fee2e5911bc59
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-07-08 19:57:05 -0400

    Added Binding and ConversionType using class enum

--- app/src/main/java/com/example/conversion_application/MainActivity.kt
+++ app/src/main/java/com/example/conversion_application/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.conversion_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.conversion_application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private enum class ConversionType {
+        CM_TO_INCH,
+        INCH_TO_CM,
+        METER_TO_FEET,
+        FEET_TO_METER,
+        KG_TO_POUND,
+        POUND_TO_KG,
+        CELSIUS_TO_FAHRENHEIT,
+        FAHRENHEIT_TO_CELSIUS,
+    }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
