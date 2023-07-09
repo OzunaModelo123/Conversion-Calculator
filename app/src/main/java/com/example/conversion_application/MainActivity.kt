2200a39a4f4e16d11537a01e420e367202b9cbe2
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-07-08 20:28:23 -0400

    Had a little issue with layouts but fixed it, added ArrayAdapter to hold the spinner layouts and conversion type(data), also set up OnItemSelectedListener

--- app/src/main/java/com/example/conversion_application/MainActivity.kt
+++ app/src/main/java/com/example/conversion_application/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.conversion_application
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import com.example.conversion_application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +26,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val conversionTypes = listOf(
+            "CM to Inch",
+            "Inch to CM",
+            "Meter to Feet",
+            "Feet to Meter",
+            "Kg to Pound",
+            "Pound to Kg",
+            "Celsius to Fahrenheit",
+            "Fahrenheit to Celsius"
+        )
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, conversionTypes)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerConversionType.adapter = adapter
+
+        binding.spinnerConversionType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var selectedConversionType = when (position) {
+                    0 -> ConversionType.CM_TO_INCH
+                    1 -> ConversionType.INCH_TO_CM
+                    2 -> ConversionType.METER_TO_FEET
+                    3 -> ConversionType.FEET_TO_METER
+                    4 -> ConversionType.KG_TO_POUND
+                    5 -> ConversionType.POUND_TO_KG
+                    6 -> ConversionType.CELSIUS_TO_FAHRENHEIT
+                    7 -> ConversionType.FAHRENHEIT_TO_CELSIUS
+                    else -> ConversionType.CM_TO_INCH // Default to CM to Inch
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Does nothing
+            }
+        }
+
     }
 }
